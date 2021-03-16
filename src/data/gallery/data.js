278948adf32693274eb6a18b737ae2732be38f23
gijs verbruggen <gijs335@hotmail.nl>
2021-03-16 20:21:29 +0100

    gallery page

---
+++ src/data/gallery/data.js
@@ -0,0 +1,52 @@
+const projects = [
+    {
+        id: '1',
+        category: 'slaves to darkness',
+        title: 'Chaos Lord',
+        date: '3-16-2021',
+        name: 'Gijs',
+        source: 'chaoslordkrakadrak.jpeg'
+    },
+    {
+        id: '2',
+        category: 'blades of khorne',
+        title: 'Slaughter Priest',
+        date: '3-16-2021',
+        name: 'Gijs',
+        source: 'slaughterpriest.jpeg'
+    },
+    {
+        id: '3',
+        category: 'slaves to darkness',
+        title: 'Varanguard',
+        date: '3-16-2021',
+        name: 'Gijs',
+        source: 'varanguardnurgle.jpeg'
+    },
+    {
+        id: '4',
+        category: 'slaves to darkness',
+        title: 'Gaunt Summoner of Tzeentch',
+        date: '3-16-2021',
+        name: 'Gijs',
+        source: 'gauntsummoneroftzeentch.jpeg'
+    },
+    {
+        id: '5',
+        category: 'Blades of Khorne',
+        title: 'Vorgaroth the Scarred',
+        date: '3-16-2021',
+        name: 'Gijs',
+        source: 'vorgaroth_the_scarred.jpeg'
+    },
+    {
+        id: '5',
+        category: 'Blades of Khorne',
+        title: 'Bloodsecrator',
+        date: '3-16-2021',
+        name: 'Gijs',
+        source: 'bloodsecrator.jpeg'
+    },
+]
+
+export default projects
--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -1,20 +1,95 @@
 <template>
   <div class="Gallery">
-    <h1>This is an about page</h1>
+    <v-container>
+      <v-row>
+        <v-col cols="12">
+          <div>
+            <v-btn @click="filter($event)" class="ma-2" outlined color="indigo">
+              All
+            </v-btn>
+            <v-btn @click="filter($event)" class="ma-2" outlined color="indigo">
+              Slaves to Darkness
+            </v-btn>
+            <v-btn @click="filter($event)" class="ma-2" outlined color="indigo">
+              Maggothkin of Nurgle
+            </v-btn>
+            <v-btn @click="filter($event)" class="ma-2" outlined color="indigo">
+              Hedonites of Slaanesh
+            </v-btn>
+            <v-btn @click="filter($event)" class="ma-2" outlined color="indigo">
+              Blades of Khorne
+            </v-btn>
+            <v-btn @click="filter($event)" class="ma-2" outlined color="indigo">
+              Disciples of Tzeentch
+            </v-btn>
+            <v-btn @click="filter($event)" class="ma-2" outlined color="indigo">
+              Beasts of Chaos
+            </v-btn>
+            <v-btn @click="filter($event)" class="ma-2" outlined color="indigo">
+              Skaven
+            </v-btn>
+          </div>
+        </v-col>
+        <v-col
+          v-for="project in filteredProjects"
+          :key="project.id"
+          class="d-flex child-flex"
+          cols="3"
+        >
+          <v-card class="ma-auto" max-width="400">
+            <v-img
+              :src="
+                require(`../../src/assets/images/gallery/${project.source}`)
+              "
+              :alt="project.title"
+              aspect-ratio="1"
+              class="grey lighten-2"
+            >
+              <template v-slot:placeholder>
+                <v-row class="fill-height ma-0" align="center" justify="center">
+                  <v-progress-circular
+                    indeterminate
+                    color="grey lighten-5"
+                  ></v-progress-circular>
+                </v-row>
+              </template>
+            </v-img>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
-
+import dataGallery from "../data/gallery/data.js";
 export default {
-  name: 'Gallery',
-
-  components: {
-
-  },
-
+  name: "Gallery",
+  components: {},
   data: () => ({
-    //
+    projects: dataGallery,
+    event: null,
+    clicked: false,
   }),
+  computed: {
+    filteredProjects() {
+      if (
+        this.event != null &&
+        this.event.target.innerText.toLowerCase() != "all"
+      ) {
+        return this.projects.filter(
+          ({ category }) =>
+            category.toLowerCase() == this.event.target.innerText.toLowerCase()
+        );
+      } else {
+        return this.projects;
+      }
+    },
+  },
+  methods: {
+    filter(event) {
+      this.event = event;
+    },
+  },
 };
 </script>
